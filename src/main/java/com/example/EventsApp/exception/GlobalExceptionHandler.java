1d336d215bb471b454a229f04b4cac7e1fbc0014
Patrycja Koscielniak <patrycja.koscielniak.eu@gmail.com>
2025-09-13 06:45:52 +0200

    Add global exception handler

---
+++ src/main/java/com/example/EventsApp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.EventsApp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity<String> handleEventNotFound(EventNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleOtherExceptions(Exception ex) {
+        return new ResponseEntity<>("Wystąpił błąd: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
